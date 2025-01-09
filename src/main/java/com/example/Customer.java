692bf0a1f6698fdcb19505522de350bfd38ce642
Sergiy Umanets <umserg@gmail.com>
2025-01-09 14:09:39 +0200

    Refactor Customer withdraw method; Extract executeWithdrawalForCustomerType method

--- src/main/java/com/example/Customer.java
+++ src/main/java/com/example/Customer.java
@@ -27,23 +27,21 @@ public class Customer {
         if (!account.getCurrency().equals(currency)) {
             throw new RuntimeException("Can't withdraw " + currency);
         }
+        executeWithdrawalForCustomerType(sum);
+    }
+
+    private void executeWithdrawalForCustomerType(double sum) {
         if (account.getType().isPremium()) {
-            switch (customerType) {
-                case COMPANY:
-                    account.setMoney(executeWithdrawal(sum, account.getMoney() < 0, companyOverdraftDiscount / 2));
-                    break;
-                case PERSON:
-                    account.setMoney(executeWithdrawal(sum, account.getMoney() < 0, 1));
-                    break;
+            if (customerType == CustomerType.COMPANY) {
+                account.setMoney(executeWithdrawal(sum, account.getMoney() < 0, companyOverdraftDiscount / 2));
+            } else {
+                account.setMoney(executeWithdrawal(sum, account.getMoney() < 0, 1));
             }
         } else {
-            switch (customerType) {
-                case COMPANY:
-                    account.setMoney(executeWithdrawal(sum, account.getMoney() < 0, companyOverdraftDiscount));
-                    break;
-                case PERSON:
-                    account.setMoney(executeWithdrawal(sum, account.getMoney() < 0, 1));
-                    break;
+            if (customerType == CustomerType.COMPANY) {
+                account.setMoney(executeWithdrawal(sum, account.getMoney() < 0, companyOverdraftDiscount));
+            } else {
+                account.setMoney(executeWithdrawal(sum, account.getMoney() < 0, 1));
             }
         }
     }
